a26f15733f074ecf036b457d19c6ab0ece91f596
SaladRaider <fakermansacc@gmail.com>
2015-06-28 16:55:33 -0700

    changed config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 //echo "Trying to connect to server...<br />";
 session_start();
-$dbc = mysql_connect('216.172.184.74:3306',"ab8072_ahs_db",'ahs@2015');
+$dbc = mysql_connect('216.172.184.74:3306',"ab8072_ahs",'ahs@2015');
 if (!$dbc)
 {
 	die('Not connected : ' . mysql_error());
@@ -12,7 +12,7 @@ else
 }
 //select database
 //echo "Trying to connect to database...<br />";
-$db_selected = mysql_select_db("ab8072_ahs",$dbc);
+$db_selected = mysql_select_db("ab8072_ahs_db",$dbc);
 if (!$db_selected)
 {
 	die("Can not connect : " . mysql_error());
