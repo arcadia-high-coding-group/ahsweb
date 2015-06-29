374b50dda126681ec93063068f9c79e2fc4097e9
SaladRaider <fakermansacc@gmail.com>
2015-06-28 17:50:06 -0700

    Added form_elements.css, changed index.php, main.css, and header.php

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
 <?php include('header.php');?>
 
-
-<div id="header">
-	<h1 id="logo">Trader</h1>
-</div>
-
 <div id="wrapper">
 
-	<p>New sdfsdf paragraph</p>
+	<input id="big-search" type='text' placeholder='Search For Volunteer Opertunity' />
+	<button id="big-search-btn">*</button>
 
-	<p>New paragraph 2</p>
 	
-	<p>another paragraph</p>
-
+	
 </div>
-
 
 <?php include('footer.php');?>
